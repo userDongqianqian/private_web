c1c2063602f073ab3e169b441fc36d38e56e5ea1
userDongqianqian <2578503280@qq.com>
2023-07-19 15:23:50 +0800

    添加加载动

--- src/components/LoadingComponent/index.tsx
+++ src/components/LoadingComponent/index.tsx
@@ -1,10 +1,16 @@
-// LoadingComponent.js
+import classes from "./index.module.less";
 import React from "react";
 
 const LoadingComponent: React.FC = () => {
   return (
-    <div className="loading-container">
-      <h2>Loading...</h2>
+    <div className={classes.loadingContainer}>
+      <div className={classes.loadingCubeContainer}>
+        <div className={`${classes.loadingCube} ${classes.loadingCube1}`}></div>
+        <div className={`${classes.loadingCube} ${classes.loadingCube2}`}></div>
+        <div className={`${classes.loadingCube} ${classes.loadingCube3}`}></div>
+        <div className={`${classes.loadingCube} ${classes.loadingCube4}`}></div>
+        <div className={`${classes.loadingCube} ${classes.loadingCube5}`}></div>
+      </div>
     </div>
   );
 };
